396d78ac06ea3d4dbedabb4dc457070d0081ef3c
Ali Salehi <alisalehi.d@gmail.com>
2025-09-10 03:16:00 +0330

    transfer money and ui part 2

--- lib/features/dashboard/domain/repositories/dashboard_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../entities/transaction.dart';
-
-abstract class DashboardRepository {
-  Future<double> getBalance();
-  Future<List<Transaction>> getTransactions({int limit = 5});
-}
--- lib/features/dashboard/presentation/bloc/dashboard_cubit.dart
+++
@@ -1,8 +0,0 @@
-// lib/src/features/dashboard/presentation/bloc/dashboard_cubit.dart
-import 'package:bloc/bloc.dart';
-
-class DashboardCubit extends Cubit<int> {
-  DashboardCubit() : super(0);
-
-  void selectTab(int index) => emit(index);
-}
